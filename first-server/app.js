9cf09d4b47a69ad8d24d02c96dc5e264b92d298a
unknown <nektariostopalides@gmail.com>
2023-06-19 11:01:54 +0300

    yo

--- first-server/app.js
+++ first-server/app.js
@@ -1,7 +1,24 @@
 const http = require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req);
-});
+const express = require('express');
+const bodyParser = require('body-parser');
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page Not Found!</h1>')
+})
+
+
+app.listen(3000);
