b7ba5bddf3b2593d7352003cbeb5c7a5ddc8a2da
NektariosTopalidis <73079858+NektariosTopalidis@users.noreply.github.com>
2023-06-27 13:02:22 +0300

    Delete first-server directory

--- first-server/util/database.js
+++
@@ -1,9 +0,0 @@
-
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node_course','nektarios','Nektarios@132', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports = sequelize;
